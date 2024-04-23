8afdded12e690cc9d280444d3ee0bfc55fa5af8d
Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-04-23 12:12:04 +0900

    add plot_paint_center in vis.py

--- src/utils/vis.py
+++ src/utils/vis.py
@@ -8,3 +8,25 @@ def plot_bbox_on_frame(frame: NDArray, bbox: NDArray) -> NDArray:
     pt2 = (bbox[2], bbox[3])
     frame = cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 2)
     return frame
+
+
+def plot_paint_center(frame, count, center):
+    center = (int(center[0]), int(center[1]))
+    frame = cv2.putText(
+        frame,
+        str(count),
+        center,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (255, 0, 0),
+        lineType=cv2.LINE_AA,
+    )
+    frame = cv2.drawMarker(
+        frame,
+        center,
+        (255, 0, 0),
+        markerType=cv2.MARKER_TILTED_CROSS,
+        markerSize=15,
+        thickness=3,
+    )
+    return frame
