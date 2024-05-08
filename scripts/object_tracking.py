3d21d34d4671f6685965052a95aba4300ac66257
Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-05-08 11:53:37 +0900

    update object_tracking.py

--- scripts/object_tracking.py
+++ scripts/object_tracking.py
@@ -32,11 +32,11 @@ for video_path in tqdm(video_paths, ncols=100):
 
     # create video writer
     if args.video:
-        yolo_video_path = os.path.join(out_dir, f"{video_name}_yolo.mp4")
+        yolo_video_path = os.path.join(out_dir, f"{video_name}_det.mp4")
         wtr = video.Writer(yolo_video_path, cap.fps, cap.size)
 
     # predict using yolo
-    yolo_rslt = []
+    det_rslt = []
     for frame_num in tqdm(
         range(cap.frame_count), ncols=100, leave=False, desc=video_name
     ):
@@ -44,16 +44,16 @@ for video_path in tqdm(video_paths, ncols=100):
         bboxs = model.predict(frame)
         for bbox in bboxs:
             frame = vis.plot_bbox_on_frame(frame, bbox)
-            yolo_rslt.append([frame_num] + bbox.tolist())
+            det_rslt.append([frame_num] + bbox.tolist())
         if args.video:
             wtr.write(frame)
 
     # save tsv
     header = "n_frame\tx1\ty1\tx2\ty2\tconf\tclass"
     fmt = ("%d", "%f", "%f", "%f", "%f", "%f", "%d")
-    yolo_tsv_path = os.path.join(out_dir, f"{video_name}_yolo.tsv")
-    np.savetxt(yolo_tsv_path, yolo_rslt, fmt, "\t", header=header, comments="")
+    det_tsv_path = os.path.join(out_dir, f"{video_name}_det.tsv")
+    np.savetxt(det_tsv_path, det_rslt, fmt, "\t", header=header, comments="")
 
-    del cap, yolo_rslt
+    del cap, det_rslt
     if args.video:
         del wtr
