083ae17b43569b182dd512a4918ae2567a9dc8af
Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-08-02 16:03:32 +0900

    update yolov8 visualization

--- src/utils/vis.py
+++ src/utils/vis.py
@@ -1,13 +1,34 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from numpy.typing import NDArray
+
+cmap = plt.get_cmap("tab10")
 
 
 def plot_bbox_on_frame(frame: NDArray, bbox: NDArray) -> NDArray:
-    bbox = bbox.astype(int)
-    pt1 = (bbox[0], bbox[1])
-    pt2 = (bbox[2], bbox[3])
-    frame = cv2.rectangle(frame, pt1, pt2, (0, 255, 0), 2)
-    frame = cv2.putText(frame, str(bbox[5]), pt1, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    x1, y1, x2, y2 = bbox[:4].astype(int)
+    pt1 = (x1, y1)
+    pt2 = (x2, y2)
+    pt_center = (x1 + (x2 - x1) // 2, y1 + (y2 - y1) // 2)
+    # score = float(bbox[4])
+    label = int(bbox[5])
+    _id = int(bbox[6])
+
+    # get color
+    c = (np.array(cmap(label)[:3]) * 255).astype(int).tolist()
+    c = tuple(c[::-1])  # RGB -> BGR
+
+    # bbox
+    frame = cv2.rectangle(frame, pt1, pt2, c, 2)
+
+    # label
+    frame = cv2.putText(frame, str(label), pt1, cv2.FONT_HERSHEY_COMPLEX, 0.7, c, 1)
+
+    # tracking ID
+    frame = cv2.putText(
+        frame, str(_id), pt_center, cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1
+    )
     return frame
 
 
