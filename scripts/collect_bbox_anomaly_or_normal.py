0e44bd12d5859d1cb99d1d60e6a1c336e894be4a
Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-10-01 09:26:28 +0900

    update collect bbox anomaly or normal

--- scripts/collect_bbox_anomaly_or_normal.py
+++ scripts/collect_bbox_anomaly_or_normal.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 import sys
 
 import cv2
@@ -34,7 +35,71 @@ def load_annotation(video_name):
     return ann_lst, n_frames
 
 
-def plot_bbox_anomaly_or_normal(video_name, th_sec, th_iou, bbox_ratio, is_finetuned):
+def is_annotation_contained_on(n_frame, ann_n_frames, th_n_frame, frame_count):
+    is_annotation_contained = False
+    for ann_n_frame in ann_n_frames:
+        th_min = max(ann_n_frame - th_n_frame, 0)
+        th_max = min(ann_n_frame + th_n_frame, frame_count)
+        if (th_min <= n_frame) and (n_frame <= th_max):
+            is_annotation_contained = True
+            break
+
+    return is_annotation_contained, ann_n_frame
+
+
+def crop_img(frame, bbox):
+    x1, y1, x2, y2 = bbox
+    h, w = frame.shape[:2]
+    x1 = max(0, x1)
+    y1 = max(0, y1)
+    x2 = min(w, x2)
+    y2 = min(h, y2)
+
+    return frame[y1:y2, x1:x2]
+
+
+def plot_save_normal_bboxs(yolo_preds, frame, n_frame, img_dir):
+    for i, pred in enumerate(yolo_preds):
+        x1, y1, x2, y2 = calc_resized_bbox(
+            pred[1:5].astype(float), bbox_ratio, frame.shape[:2]
+        )
+        img = crop_img(frame, (x1, y1, x2, y2))
+        img_path = f"{img_dir}/{n_frame}_{i}.jpg"
+        cv2.imwrite(img_path, img)
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+
+    return frame
+
+
+def plot_save_anomaly_bboxs(labels, yolo_preds, frame, n_frame, img_dir):
+    for i, (pred, label) in enumerate(zip(yolo_preds, labels)):
+        x1, y1, x2, y2 = calc_resized_bbox(
+            pred[1:5].astype(float), bbox_ratio, frame.shape[:2]
+        )
+        img = crop_img(frame, (x1, y1, x2, y2))
+        img_path = f"{img_dir}/{label}/{n_frame}_{i}.jpg"
+        if not os.path.exists(os.path.dirname(img_path)):
+            os.makedirs(os.path.dirname(img_path))
+        cv2.imwrite(img_path, img)
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+        frame = cv2.putText(
+            frame,
+            str(label),
+            (x1, y1),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.7,
+            (255, 0, 0),
+            1,
+        )
+
+    return frame
+
+
+def collect_bbox_anomaly_or_normal(
+    video_name, th_sec, th_iou, bbox_ratio, is_finetuned, img_dir="datasets/images/"
+):
     if is_finetuned:
         str_finetuned = "_finetuned"
     else:
@@ -56,7 +121,6 @@ def plot_bbox_anomaly_or_normal(video_name, th_sec, th_iou, bbox_ratio, is_finet
     # load video
     cap = video.Capture(f"video/{video_name}.mp4")
     frame_count = cap.frame_count
-    frame_size = cap.size
     th_n_frame = np.ceil(cap.fps * th_sec).astype(int)
 
     # create writer
@@ -66,43 +130,54 @@ def plot_bbox_anomaly_or_normal(video_name, th_sec, th_iou, bbox_ratio, is_finet
         cap.size,
     )
 
+    # create save folder
+    img_dir = f"{img_dir}/sec{th_sec}_iou{th_iou}_br{bbox_ratio}/{video_name}"
+    if os.path.exists(img_dir):
+        shutil.rmtree(img_dir)
+    os.makedirs(img_dir)
+    img_dir_normal = f"{img_dir}/0"
+    os.makedirs(img_dir_normal)
+    img_dir_anomaly = f"{img_dir}/1"
+    os.makedirs(img_dir_anomaly)
+
     for n_frame in tqdm(range(frame_count), ncols=100, desc=video_name):
         ret, frame = cap.read()
         if not ret:
             print(f"frame not loaded from n_frame {n_frame} in {video_name}.mp4")
             continue
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         yolo_preds_tmp = yolo_preds[yolo_preds.T[0].astype(int) == n_frame]
         if len(yolo_preds_tmp) == 0:
-            wrt.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            wrt.write(frame)
             continue
 
         # frame contains abnormal labels?
-        is_annotation_contained = False
-        for ann_n_frame in ann_n_frames:
-            th_min = max(ann_n_frame - th_n_frame, 0)
-            th_max = min(ann_n_frame + th_n_frame, frame_count)
-            if (th_min <= n_frame) and (n_frame <= th_max):
-                is_annotation_contained = True
-                break
+        is_annotation_contained, ann_n_frame = is_annotation_contained_on(
+            n_frame, ann_n_frames, th_n_frame, frame_count
+        )
 
         if not is_annotation_contained:
             # annotation is not found in this frame
-            # plot normal data
-            for pred in yolo_preds_tmp:
-                x1, y1, x2, y2 = calc_resized_bbox(
-                    pred[1:5].astype(float), bbox_ratio, frame_size
-                )
-                frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            frame = plot_save_normal_bboxs(
+                yolo_preds_tmp, frame, n_frame, img_dir_normal
+            )
         else:
             ann_lst_tmp = ann_lst[ann_lst.T[0].astype(int) == ann_n_frame]
 
             # calc iou
+            labels = []
             ious = []
             for ann in ann_lst_tmp:
                 ann_bbox = ann[1:5].astype(np.float32)
                 label = ann[8]
+                try:
+                    label = label.split("(")[1].replace(
+                        ")", ""
+                    )  # extract within bracket
+                except IndexError:
+                    print("error label", video_name, label)
+                    continue
+                labels.append(label)
 
                 # plot annotation
                 x1, y1, x2, y2 = ann_bbox.astype(int)
@@ -123,38 +198,29 @@ def plot_bbox_anomaly_or_normal(video_name, th_sec, th_iou, bbox_ratio, is_finet
             if len(ious) == 0:
                 raise ValueError("not found annotation")
             elif len(ious) == 1:
-                ious = ious[0]
+                ious = ious[0].ravel()
+                labels = np.array([labels[0] for _ in range(len(ious))])
             else:
                 ious = np.concatenate(ious, axis=1)
 
-            mask = np.any(ious >= th_iou, axis=1)
+                # select the label which has the highest iou score
+                labels = np.choose(np.argmax(ious, axis=1), labels)
+                ious = np.max(ious, axis=1)
+
+            mask = ious >= th_iou
             yolo_preds_low_iou = yolo_preds_tmp[~mask]
             yolo_preds_high_iou = yolo_preds_tmp[mask]
+            labels = labels[mask]
 
-            # plot normal data
-            for pred in yolo_preds_low_iou:
-                x1, y1, x2, y2 = calc_resized_bbox(
-                    pred[1:5].astype(float), bbox_ratio, frame_size
-                )
-                frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            frame = plot_save_normal_bboxs(
+                yolo_preds_low_iou, frame, n_frame, img_dir_normal
+            )
 
-            # plot anomaly data
-            for pred in yolo_preds_high_iou:
-                x1, y1, x2, y2 = calc_resized_bbox(
-                    pred[1:5].astype(float), bbox_ratio, frame_size
-                )
-                frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
-                frame = cv2.putText(
-                    frame,
-                    str(label),
-                    (x1, y1),
-                    cv2.FONT_HERSHEY_COMPLEX,
-                    0.7,
-                    (255, 0, 0),
-                    1,
-                )
+            frame = plot_save_anomaly_bboxs(
+                labels, yolo_preds_high_iou, frame, n_frame, img_dir_anomaly
+            )
 
-        wrt.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        wrt.write(frame)
 
     del cap, wrt
 
@@ -198,7 +264,7 @@ if __name__ == "__main__":
             continue
 
         video_name = video_id_to_name[video_id]
-        plot_bbox_anomaly_or_normal(
+        collect_bbox_anomaly_or_normal(
             video_name, th_sec, th_iou, bbox_ratio, args.finetuned_model
         )
     print("complete")
