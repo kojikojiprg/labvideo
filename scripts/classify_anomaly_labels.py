f0c9034124089f0aa9b4569efd15963e03725439
Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2024-11-07 11:24:30 +0900

    update classify anomaly labels

--- scripts/classify_anomaly_labels.py
+++ scripts/classify_anomaly_labels.py
@@ -46,10 +46,13 @@ if __name__ == "__main__":
     data_name = f"sec{th_sec}_iou{th_iou}_br{bbox_ratio}"
     dataset_dir = f"datasets/classify/{data_name}/{split_type}/{data_type}"
     yolo_result_dir = f"runs/classify/{data_name}/{split_type}/{data_type}"
-    os.makedirs(dataset_dir, exist_ok=True)
 
     # create dataset
     if args.create_dataset:
+        if os.path.exists(dataset_dir):
+            shutil.rmtree(dataset_dir)
+        os.makedirs(dataset_dir, exist_ok=False)
+
         ann_json = json_handler.load("annotation/annotation.json")
         info_json = json_handler.load("annotation/info.json")
         video_id_to_name = {
@@ -94,34 +97,34 @@ if __name__ == "__main__":
         create_classification_dataset(data, train_idxs, dataset_dir, data_type, "train")
         create_classification_dataset(data, test_idxs, dataset_dir, data_type, "test")
 
-    # if args.train:
-    #     # train YOLO
-    #     yolo_result_dir = train_classify(data_name, data_type, split_type)
-    # else:
-    #     # only prediction
-    #     v_num = args.version
-    #     if v_num is not None:
-    #         yolo_result_dir += f"-v{v_num}"
+    if args.train:
+        # train YOLO
+        yolo_result_dir = train_classify(data_name, data_type, split_type)
+    else:
+        # only prediction
+        v_num = args.version
+        if v_num is not None:
+            yolo_result_dir += f"-v{v_num}"
 
-    # # prediction
-    # train_paths = glob(os.path.join(dataset_dir, "train", "**", "*.jpg"))
-    # test_paths = glob(os.path.join(dataset_dir, "test", "**", "*.jpg"))
+    # prediction
+    train_paths = glob(os.path.join(dataset_dir, "train", "**", "*.jpg"))
+    test_paths = glob(os.path.join(dataset_dir, "test", "**", "*.jpg"))
 
-    # train_labels = np.loadtxt(f"{dataset_dir}/summary_train.tsv", str, delimiter="\t")[
-    #     :-1
-    # ].T[0]
-    # test_labels = np.loadtxt(f"{dataset_dir}/summary_test.tsv", str, delimiter="\t")[
-    #     :-1
-    # ].T[0]
-    # labels = train_labels.tolist() + test_labels.tolist()
-    # labels = np.unique(sorted(labels))
+    train_labels = np.loadtxt(f"{dataset_dir}/summary_train.tsv", str, delimiter="\t")[
+        :-1
+    ].T[0]
+    test_labels = np.loadtxt(f"{dataset_dir}/summary_test.tsv", str, delimiter="\t")[
+        :-1
+    ].T[0]
+    labels = train_labels.tolist() + test_labels.tolist()
+    labels = np.unique(sorted(labels))
 
-    # results_train, missed_img_path_train = pred_classify(
-    #     train_paths, "train", yolo_result_dir, labels
-    # )
-    # results_test, missed_img_path_test = pred_classify(
-    #     test_paths, "test", yolo_result_dir, labels
-    # )
+    results_train, missed_img_path_train = pred_classify(
+        train_paths, "train", yolo_result_dir, labels
+    )
+    results_test, missed_img_path_test = pred_classify(
+        test_paths, "test", yolo_result_dir, labels
+    )
 
     # missed_imgs_dir = os.path.join(yolo_result_dir, "missed_images_test")
     # if os.path.exists(missed_imgs_dir):
